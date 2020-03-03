56b4d4dd6ed23e64b69811ac6ea6cf50d7677aa3
Kristian <kristian.roopnarine@gmail.com>
2020-03-03 18:02:32 -0500

    Added book list view.

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Book
 # Create your views here.
+
+def book_list_view(request):
+    context={}
+    context['queryset'] = Book.objects.all()
+    return render(request,'marketplace/book_list.html',context)

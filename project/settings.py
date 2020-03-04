035fb131cba73a68c7b921c3d8880cdb0cf6e685
Kristian <kristian.roopnarine@gmail.com>
2020-03-03 21:21:33 -0500

    Fixing all auth forms.

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'marketplace'
 ]
 
@@ -126,3 +130,13 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media_root')
+
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
--- project/urls.py
+++ project/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('marketplace.urls'),name="marketplace")
+    path('accounts/',include('allauth.urls')),
+    path('',include('marketplace.urls'),name="marketplace"),
 
 ]
 
